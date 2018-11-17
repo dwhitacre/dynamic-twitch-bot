61e3ff69e94e6a9c61262660a30015bf23bcbb9e
danwhitacre <daniel.whitacre@ibm.com>
2018-11-17 15:05:59 -0500

    init

---
+++ example/example.js
@@ -0,0 +1,3 @@
+const DynamicTwitchBot = require('../index.js');
+const creds = require('./creds.json');
+
---
+++ index.js
@@ -0,0 +1,7 @@
+class DynamicTwitchBot {
+  constructor() {
+
+  }
+}
+
+module.exports = DynamicTwitchBot;
